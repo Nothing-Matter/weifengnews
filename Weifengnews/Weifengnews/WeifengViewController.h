b6d740a68d6d63801a4b4a66f8d7a5724acd45c8
Nothing- <qzadsl1024@qq.com>
2013-07-16 11:59:54 +0800

    项目框架

---
+++ Weifengnews/Weifengnews/WeifengViewController.h
@@ -0,0 +1,13 @@
+//
+//  WeifengViewController.h
+//  Weifengnews
+//
+//  Created by ibokan on 13-7-16.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeifengViewController : UINavigationController
+
+@end
